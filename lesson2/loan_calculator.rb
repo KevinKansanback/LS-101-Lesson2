90e4ee4cee305507cefd3b726b647d6006848a07
Kevin Kansanback <Kevin.Kansanback@gmail.com>
2019-06-21 21:03:55 +0000

    refactored loan calculator

--- lesson2/loan_calculator.rb
+++ lesson2/loan_calculator.rb
@@ -1,9 +1,3 @@
-# prompt for: loan amount, APR, loan duration (each separate variable) nesting
-#   validate each entry
-# calculate Monthly interest and loan duration in months
-# use: m = p * (j / (1 -(1+j)**(-n))))
-
-# run code through rubocop
 require 'yaml'
 
 MESSAGES = YAML.load_file('loan_calculator_messages.yml')
@@ -16,6 +10,28 @@ def prompt(message)
   puts ">>> #{message}"
 end
 
+def retrieve_name()
+  name_entry = ''
+  loop do
+    name_entry = gets.chomp
+    if invalid_name_input?(name_entry)
+      prompt(MESSAGES['valid_name'])
+      prompt(MESSAGES['welcome'])
+    else
+      break
+    end
+  end
+  name_entry
+end
+  
+def invalid_name_input?(input)
+  if input.empty? || input.to_i != 0 || input.start_with?(' ')
+    true
+  elsif input.to_i == 0 || input.to_f == 0.0
+    true
+  end
+end
+
 def valid_integer?(integer)
   integer.to_i.to_s == integer
 end
@@ -24,11 +40,23 @@ def valid_float?(float)
   float.to_f.to_s == float
 end
 
-def valid_number?(num)
+def valid_num?(num)
   valid_float?(num) || valid_integer?(num)
 end
 
-def validate_positive_number?(num)
+def valid_loan_amt_entry?(num)
+  valid_float?(num) || valid_integer?(num)
+end
+
+def valid_apr_entry?(num)
+  valid_float?(num) || valid_integer?(num)
+end
+
+def valid_loan_dur_entry?(num)
+  valid_float?(num) || valid_integer?(num)
+end
+
+def valid_pos_num?(num)
   (num.to_i > 0 || num.to_f > 0.0)
 end
 
@@ -39,107 +67,106 @@ def string_convert_to_num(string)
     string.to_f
   end
 end
-# START
-# welcome user to loan calculator
-prompt(MESSAGES['welcome'])
-name = ''
-loop do
-  name = gets.chomp
 
-  if name.empty? || name.to_i != 0
-    prompt(MESSAGES['valid_name'])
-    prompt(MESSAGES['welcome'])
-  else
-    break
-  end
-end
-
-prompt("Hi #{name}, I hope you enjoy your experience today!")
-
-loop do # main loop
-
-  # get loan amount
-  prompt(MESSAGES['loan_amount_message'])
-  loan_amount = ''
-  #  use loop to validate
+def retrieve_loan_amount()
   loop do
-    loan_amount = gets.chomp
+    loan_amt_in = gets.chomp
 
-    if valid_number?(loan_amount) && validate_positive_number?(loan_amount)
-      loan_amount = string_convert_to_num(loan_amount)
-    break
+    if valid_loan_amt_entry?(loan_amt_in) && valid_pos_num?(loan_amt_in)
+      return string_convert_to_num(loan_amt_in)
     else
       prompt(MESSAGES['valid_loan_number'])
     end
   end
+end
 
-  # prompt user for APR
-  prompt(MESSAGES['apr_message'])
-
-  apr_rate = ''
-
+def retreive_apr()
   loop do
-    apr_rate = gets.chomp
+    retreive_apr_rate = gets.chomp
 
-    if valid_number?(apr_rate) && validate_positive_number?(apr_rate)
-      apr_rate = string_convert_to_num(apr_rate).to_f / 100 / 12
-      break
+    if valid_apr_entry?(retreive_apr_rate) && valid_pos_num?(retreive_apr_rate)
+      return string_convert_to_num(retreive_apr_rate).to_f / 100 / 12
     else
       prompt(MESSAGES['valid_apr_number'])
     end
   end
+end
 
-  # prompt user for loan duration in months and save into variable
-  prompt(MESSAGES['loan_duration_message'])
-
-  loan_duration = ''
-
-  #  use loop to validate
+def retrieve_loan_duration()
   loop do
-    loan_duration = gets.chomp
-    if valid_number?(loan_duration) && validate_positive_number?(loan_duration)
-      loan_duration = string_convert_to_num(loan_duration)
-      break
+    loan_dur = gets.chomp
+    
+    if valid_loan_dur_entry?(loan_dur) && valid_pos_num?(loan_dur)
+      return string_convert_to_num(loan_dur)
     else
       prompt(MESSAGES['valid_loan_duration'])
     end
   end
-  # perform calculation and save it to a var monthly_payment
+end
 
-  monthly_payment = loan_calculate(loan_amount, apr_rate, loan_duration)
-  monthly_payment = monthly_payment.round(2).to_f
+def display_monthly_payment(calculated_payment)
+  payment = format('%02.f', calculated_payment)
+  prompt("Your monthly payment is $#{payment}, not too bad!")
+end
 
-  # display result of calculation
-  prompt("Your monthly payment is $#{format('%02.2f', monthly_payment)}, not too bad!")
+def valid_new_calc_answer?(user_input)
+  valid_entry = ['y', 'yes', 'n', 'no']
+  if valid_entry.include?(user_input)
+    true
+  end
+end
 
-  # ask to do it again
-  prompt(MESSAGES['perform_again'])
-
-  # loop to validate - only accepting - y, yes, n, no
-
+def retrieve_new_calc_answer
   answer = ''
   loop do
     answer = gets.chomp.downcase
-    case answer
-    when "y"
-      answer = 'y'
-      break
-    when "yes"
-      answer = 'y'
-      break
-    when 'n'
-      answer = 'n'
-      break
-    when 'no'
-      answer = 'n'
-      break
-    when 'the jungle book'
-      prompt(MESSAGES['jungle_book'])
-    else
-      prompt(MESSAGES['perform_again_invalid'])
-    end
+    break if valid_new_calc_answer?(answer)
+    display_invalid_recalc_answer_message(answer)
   end
-  break unless answer == 'y' || answer == 'yes'
+  answer
 end
-# say goodbye
+
+def display_invalid_recalc_answer_message(answer)
+  if answer == 'the jungle book'
+    prompt(MESSAGES['jungle_book'])
+  else
+    prompt(MESSAGES['perform_again_invalid'])
+  end
+end
+  
+def new_calculation?(answer)
+  if answer == 'y' || answer == 'yes'
+    true
+  end
+end
+  
+# START
+prompt(MESSAGES['welcome'])
+
+name = retrieve_name()
+prompt("Hi #{name}, I hope you enjoy your experience today!")
+
+loop do # main loop
+  prompt(MESSAGES['loan_amount_message'])
+  
+  loan_amount = retrieve_loan_amount()
+  
+  prompt(MESSAGES['apr_message'])
+
+  apr_rate = retreive_apr()
+
+  prompt(MESSAGES['loan_duration_message'])
+
+  loan_duration = retrieve_loan_duration()
+
+  monthly_payment = loan_calculate(loan_amount, apr_rate, loan_duration)
+  
+  display_monthly_payment(monthly_payment)
+
+  prompt(MESSAGES['perform_again'])
+
+  recalc_answer = retrieve_new_calc_answer
+  
+  break unless new_calculation?(recalc_answer)
+end
 prompt(MESSAGES['end_program'])
