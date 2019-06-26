e669325d416296e7859c1c25bdf8426bf16c6c50
Kevin Kansanback <Kevin.Kansanback@gmail.com>
2019-06-26 22:39:25 +0000

    RPS code walkthrough - refactored

---
+++ lesson2/rock_paper_scissors.rb
@@ -0,0 +1,45 @@
+VALID_CHOICES = %w(rock paper scissors)
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: rock, paper, scissors")
+    choice = gets.chomp
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+prompt('thank you for playing, good bye!')
