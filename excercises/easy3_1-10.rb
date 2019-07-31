eae0071af80c4f19466ad494df6f9e4d0ce64290
Kevin Kansanback <Kevin.Kansanback@gmail.com>
2019-07-31 22:17:33 +0000

    excercises commit

--- excercises/easy3_1-10.rb
+++ excercises/easy3_1-10.rb
@@ -31,3 +31,57 @@
 # puts "#{first_num} / #{second_num} = #{first_num / second_num}"
 # puts "#{first_num} ** #{second_num} = #{first_num ** second_num}"
 # puts "#{first_num} % #{second_num} = #{first_num % second_num}"
+
+# # 3
+# puts "Please give me a word or words"
+# word_or_words = gets.chomp.downcase
+# puts "There are #{word_or_words.split.join.length} chars in #{word_or_words}."
+
+# # 4
+# def multiply(num1, num2)
+#   num1 * num2
+# end
+
+# # 5
+# def square (num)
+#   multiply(num, num)
+# end
+# puts square 3
+
+# # 6
+# def xor?(exp1, exp2)
+#   if exp1 && exp2 == true || exp1 && exp2 == false
+#     false
+#   else
+#     true
+#   end
+# end
+
+# # 7
+# def oddities(arr)
+#   odd_array = []
+#   index = 0
+#   while index < arr.length
+#     odd_array << arr[index]
+#     index +=2
+#   end
+#   odd_array
+# end
+
+# p oddities(["game", 'of', 'thrones'])
+
+# # 8
+# def palindrome?(inp)
+#   inp == inp.reverse
+# end
+
+# # 9
+# def real_palindrome?(str)
+#   str = str.downcase.delete('^a-z0-9')
+#   palindrome?(str)
+# end
+
+# 10
+def num_palindrome?(num)
+  palindrome?(num.to_s)
+end
